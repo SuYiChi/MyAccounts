70cd5afd94462896449cb21d9811644a680076fe
JamesWang <jame2408@gmail.com>
2018-07-08 18:44:25 +0800

    依據設計稿，完成畫面下半部條列清單。

--- MyAccounts/Controllers/MoneyController.cs
+++ MyAccounts/Controllers/MoneyController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyAccounts.Enum;
+using MyAccounts.Models.ViewModel;
 
 namespace MyAccounts.Controllers
 {
@@ -12,6 +14,34 @@ namespace MyAccounts.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult MoneyReport()
+        {
+            var model = GetMoneyReport();
+            return View(model);
+        }
+
+        /// <summary>
+        /// 取得資料（目前為寫死假資料，以後要串資料庫資料改這邊就好）
+        /// </summary>
+        /// <param name="model"></param>
+        private List<MoneyViewModel> GetMoneyReport()
+        {
+            var model = new List<MoneyViewModel>();
+            for (var i = 0; i < 50; i++)
+            {
+                var fake = new MoneyViewModel
+                {
+                    Category = i % 2 == 0 ? Balance.支出 : Balance.收入,
+                    Date = DateTime.Today.AddDays(-i),
+                    Amount = i * 50 + 10
+                };
+                model.Add(fake);
+            }
+
+            return model;
         }
     }
 }
