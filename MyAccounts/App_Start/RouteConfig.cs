8794f9419c816bbf7c6a3db9f5233a1d8e2ba435
JamesWang <jame2408@gmail.com>
2018-07-08 19:56:38 +0800

    調整 Route 與超連結，將做好的記帳本設為首頁。

--- MyAccounts/App_Start/RouteConfig.cs
+++ MyAccounts/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MyAccounts
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Money", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
