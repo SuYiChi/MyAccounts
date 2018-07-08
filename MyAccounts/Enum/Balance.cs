e94b1b78922b3b4107aface7b465252171b0fdd5
JamesWang <jame2408@gmail.com>
2018-07-08 17:54:00 +0800

    建立 ViewModel，快速產生 Controller 與 View (尚未套版)。

---
+++ MyAccounts/Enum/Balance.cs
@@ -0,0 +1,8 @@
+﻿namespace MyAccounts.Enum
+{
+    public enum Balance
+    {
+        支出 = 1,
+        收入 = 2
+    }
+}
---
+++ MyAccounts/Models/ViewModel/MoneyViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using MyAccounts.Enum;
+
+namespace MyAccounts.Models.ViewModel
+{
+    public class MoneyViewModel
+    {
+        [DisplayName("類別")]
+        public Balance Category { get; set; }
+        [DisplayName("金額")]
+        public int Amount { get; set; }
+        [DisplayName("日期")]
+        public DateTime Date { get; set; }
+        [DisplayName("備註")]
+        public string Description { get; set; }
+    }
+}
