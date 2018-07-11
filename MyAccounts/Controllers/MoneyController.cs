3a2a40f54c90d1bf989750a296b782fe2cf1eefb
JamesWang <jame2408@gmail.com>
2018-07-11 20:36:38 +0800

    調整另外一種寫法：利用 yield return 回傳假資料。

--- MyAccounts/Controllers/MoneyController.cs
+++ MyAccounts/Controllers/MoneyController.cs
@@ -27,21 +27,17 @@ namespace MyAccounts.Controllers
         /// 取得資料（目前為寫死假資料，以後要串資料庫資料改這邊就好）
         /// </summary>
         /// <param name="model"></param>
-        private List<MoneyViewModel> GetMoneyReport()
+        private IEnumerable<MoneyViewModel> GetMoneyReport()
         {
-            var model = new List<MoneyViewModel>();
             for (var i = 0; i < 50; i++)
             {
-                var fake = new MoneyViewModel
+                yield return new MoneyViewModel()
                 {
                     Category = i % 2 == 0 ? Balance.支出 : Balance.收入,
                     Date = DateTime.Today.AddDays(-i),
                     Amount = i * 50 + 10
                 };
-                model.Add(fake);
             }
-
-            return model;
         }
     }
 }
