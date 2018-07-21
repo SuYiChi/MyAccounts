279b32179971822d7d994d37a27dca76c4274f7d
JamesWang <jame2408@gmail.com>
2018-07-21 09:19:35 +0800

    調整自定驗證，增加 sealed 與 value == null 判斷。

--- MyAccounts/Models/CustomVaildAttributes/MaxDateIsTodayAttribute.cs
+++ MyAccounts/Models/CustomVaildAttributes/MaxDateIsTodayAttribute.cs
@@ -3,10 +3,16 @@ using System.ComponentModel.DataAnnotations;
 
 namespace MyAccounts.Models.CustomVaildAttributes
 {
-    public class MaxDateIsTodayAttribute : ValidationAttribute
+    public sealed class MaxDateIsTodayAttribute : ValidationAttribute
     {
         public override bool IsValid(object value)
         {
+            // 權責分清楚，沒輸入在這個驗證中不算錯。
+            if (value == null)
+            {
+                return true;
+            }
+
             var compareDate = value as DateTime?;
             if (compareDate.HasValue)
             {
